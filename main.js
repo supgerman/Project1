0990d6b3ecd2e92d1f25b250cc779e9e8077a3fa
Anna <a.gondring@gmail.com>
2018-11-12 19:17:00 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Venue 4 You</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> 
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+   
+    <header>
+    <h1>Venue 4 you</h1>
+    <p></p>
+</header>
+<hr>
+
+
+
+    <a href='https://www.freepik.com/free-vector/abstract-triangle-shape-burst-background_2395693.htm'>Designed by Starline</a>
+</body>
+</html>
