8ed7389be62a30a49479dd2485f2ea81920353a1
mshiwratan <monicashiwratan@gmail.com>
2018-11-18 00:47:34 -0500

    Corrected initial map display to reflect map of NYC.

--- mapScript.js
+++ mapScript.js
@@ -1,21 +1,25 @@
-//DEFAULT ZIP CODE AND LAT/LONG: This is the zip code and lat/long that will be displayed when the user first navigates to the page.
-//These variables will be updated once the user enters the zip code of their choice.
-var zipCode = "";
-var mapLongitude = "";
-var mapLatitude = "";
+//MAP OBJECT: Used to render the initial map of NYC.
+var map = L.map("mapid").setView([40.77,-73.99], 12);
 
+//ACCESS TOKEN: This is what will be used by the Ajax call to retrive the lat/long for the user entered zip code.
+var accessToken = 'sk.eyJ1IjoibXNoaXdyYXRhbjg4IiwiYSI6ImNqb2Ywc2g2NzAydHcza2xqNzMyMXE1N3gifQ.245Qhtto4LSUwvYByTpZmg';
 
-
-
-//MAP OBJECT
-var map = L.map("mapid").setView([0, 0], 15);
-
-
+//LEAFLET METHOD FOR MAP DISPLAY: This uses the map object mentioned above in the method addTo(map). 
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + accessToken, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets',
+}).addTo(map);
 
 
 //MAP RENDERING: This is the code that must be called for mapped location to be displayed to the page. User entered zip code is captured here.
 //The lat/long for the zip code is obtained here from an AJAX call. 
 function renderMap() {
+
+    //DEFAULT ZIP CODE AND LAT/LONG: This is the zip code and lat/long that will be displayed when the user first navigates to the page.
+    //These variables will be updated once the user enters the zip code of their choice.
+    var zipCode = "";
+    var mapLongitude = "";
+    var mapLatitude = "";
 
     //ON CLICK EVENT FOR ZIPCODE SUBMIT BUTTON
     $("#zipCodeSubmit").on("click", function (event) {                          //1.Submit button is clicked.
@@ -24,21 +28,12 @@ function renderMap() {
         zipCode = enteredZipCode;                                               //4.Default zip code will be updated with the user entered zip code.
         $("#zipCodeInput").val("");                                             //5.Zip code text box on the html page will be cleared.
 
-        //ACCESS TOKEN AND QUERY URL: This is what will be used by the Ajax call to retrive the lat/long for the user entered zip code.
-        var accessToken = 'sk.eyJ1IjoibXNoaXdyYXRhbjg4IiwiYSI6ImNqb2Ywc2g2NzAydHcza2xqNzMyMXE1N3gifQ.245Qhtto4LSUwvYByTpZmg';
-
+        //QUERY URL
         var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
             + zipCode
             + ".json?access_token="
             + accessToken
             + "&cachebuster=1542175693377&autocomplete=true&types=postcode&limit=1"
-
-        //LEAFLET METHOD FOR MAP DISPLAY
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + accessToken, {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox.streets',
-        }).addTo(map);
-
 
         //AJAX CALL BEGINS HERE    
         $.ajax({
